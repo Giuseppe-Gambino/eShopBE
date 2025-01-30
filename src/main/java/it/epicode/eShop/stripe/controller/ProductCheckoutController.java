e6968e7e602c5c45f47ee1aafabdfb0b7750ca8f
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-01-30 17:37:51 +0100

    prima configurazione pagamenti stripe

---
+++ src/main/java/it/epicode/eShop/stripe/controller/ProductCheckoutController.java
@@ -0,0 +1,34 @@
+package it.epicode.eShop.stripe.controller;
+
+
+
+import it.epicode.eShop.stripe.dto.ProductRequest;
+
+import it.epicode.eShop.stripe.dto.StripeResponse;
+import it.epicode.eShop.stripe.service.StripeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product/v1")
+public class ProductCheckoutController {
+
+
+    private StripeService stripeService;
+
+    public ProductCheckoutController(StripeService stripeService) {
+        this.stripeService = stripeService;
+    }
+
+    @PostMapping("/checkout")
+    public ResponseEntity<StripeResponse> checkoutProducts(@RequestBody ProductRequest productRequest) {
+        StripeResponse stripeResponse = stripeService.checkoutProducts(productRequest);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(stripeResponse);
+    }
+}
